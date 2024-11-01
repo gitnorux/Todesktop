dee2bb2733f7a609725d204cd6e455e4f1a3864c
kanchan jaiswal <164477406+gitnorux@users.noreply.github.com>
2024-11-01 19:34:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      // colors: {
+      //   primary: "blue",
+      // },
+      // fontFamily: {
+      //   display: ["Poppins", "sans-serif"],
+      //   body: ["Inter", "sans-serif"],
+      // },
+    },
+  },
+  plugins: [],
+};
